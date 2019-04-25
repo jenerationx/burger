ce7dd47fa0dd80545bdd0901130d78de9643a6ff
jenerationx <jenerationx74@gmail.com>
2019-04-24 23:14:40 -0400

    setup done

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+### Seeds
+INSERT INTO burgers (burger_name) VALUES ("Single"), 
+("Double"), 
+("Single with Bacon"),
+("Double with Bacon"), 
+("Turkey Burger"), 
+("Vegan Chickpea Burger"),
+("Bacon Cheddar Chicken Burger");
