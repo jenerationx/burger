515ab727355384520120f089fc8ee32e0803a0e4
jenerationx <jenerationx74@gmail.com>
2019-04-27 15:03:37 -0400

    update readme

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $("#devour").on("click", function (event) {
+  $(".devour").on("click", function (event) {
     var id = $(this).data("id");
     var newDevoured = $(this).data("newdevoured");
     var newState = {
@@ -18,7 +18,6 @@ $(function () {
       }
     );
   });
-
 
   $("#add-burger").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
